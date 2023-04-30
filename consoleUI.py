a64f8394ec314ebb78f5c496f6de8099d6e49ca6
KarimMaged <karim.maged2020@yahoo.com>
2023-04-30 15:39:28 +0300

    Begin the logged function and finish create project, vew all projects, and view user projects functionalities

--- consoleUI.py
+++ consoleUI.py
@@ -1,4 +1,74 @@
-import user, project, re, ast
+import user, project, re, ast, datetime
+
+def logged(logged_user):
+    project.Project.load()
+    while True:
+        print(f"Welcome to your profile {logged_user.first_name} {logged_user.last_name}")
+        print('here is the main menu')
+        print('1-Create a Project')
+        print('2-view your projects')
+        print('3-Modify Your project(s)')
+        print('4-See All avaialable Projects')
+        print('5-Donate to a project')
+        print('6-Log Out')
+        choice = input()
+        # create a project
+        if choice == '1':
+            # get title
+            while True:
+              title = input('enter the title of the project\n')
+              if title != "":
+                  break
+            # get details
+            while True:
+              details = input('type the details about the project\n')
+              if details != "":
+                  break
+            # get target
+            while True:
+              target = input('type the target of the project,it must be numbers\n')
+              if re.fullmatch('[0-9]+',target):
+                  break
+            # get start date
+            while True:
+              start = input('type the date using this format yy,mm,dd and exclude leading zero eg: 2023,5,25\n')
+              if start != "":
+                  break
+              
+            # get end date
+            while True:
+              end = input('type the date using this format yy,mm,dd and exclude leading zero eg: 2023,5,25\n')
+              if end != "":
+                  break
+            start_arr = start.split(',')
+            end_arr = end.split(',')
+            project.Project(title,details,int(target),datetime.date(int(start_arr[0]),int(start_arr[1]),int(start_arr[2])),datetime.date(int(end_arr[0]),int(end_arr[1]),int(end_arr[2])),logged_user)
+            project.Project.save()
+         
+
+        # View your projects
+        elif choice == '2':
+            project.Project.all_projects = []
+            project.Project.load()
+            project.Project.display_user_projects(logged_user)
+        # modify your projects
+        elif choice == '3':
+            pass
+        # see all available projects
+        elif choice == '4':
+            project.Project.all_projects = []
+            project.Project.load()
+            project.Project.display_all_projects()
+        # Donate to a project
+        elif choice == '5':
+            pass
+        # Log out
+        elif choice == '6':
+            break
+        else:
+             print ('the input you provided is invalid. please choose from the menu')
+             continue
+        
 
 def login():
     while True:
@@ -6,8 +76,9 @@ def login():
         print ('Do you want to login or create a new account')
         print('1-LogIn')
         print('2-Create a new account')
+        print('3-Exit')
         reply = input()
-        if reply != '1' and reply != '2':
+        if reply != '1' and reply != '2' and reply != '3':
             print ('the input you provided is invalid. please choose from the menu')
             continue
         
@@ -20,17 +91,25 @@ def login():
             for line in users:
                 user_dict = ast.literal_eval(line)
                 if user_dict['email'] == mail:
-                    current_user = user_dict
+                    current_user = user.User()
+                    current_user.set_first_name(user_dict['first_name'])
+                    current_user.set_last_name(user_dict['last_name'])
+                    current_user.set_email(user_dict['email'])
+                    current_user.set_password(user_dict['password'])
+                    current_user.set_phone(user_dict['phone'])
+                    break
             else:
                 print('This Email does not Exist')
             
             if current_user:
-                input_password = input (f"welcome {current_user['first_name']} Please enter your password\n")
-                for i in range(2):
-                    if input_password == current_user['password']:
-                        print ('you logged in successfully')
-                    else:
-                      input_password = input ('The password you entered is not correct. please re-enter the password\n')  
+                print(current_user)
+                input_password = input (f"welcome {current_user.first_name} Please enter your password\n")
+        
+                if input_password == current_user.password:
+                    print ('you logged in successfully\n')
+                    logged(current_user)
+                else:
+                    input_password = input ('The password you entered is not correct. press Enter to continue\n')  
 
 
         # if user chose to create an account
@@ -78,8 +157,11 @@ def login():
             u.add_instance()
             print(user.User.instances)
             u.save()
-            print ('your account was created successfully. Now you have to login\n')        
+            print ('your account was created successfully. Now you have to login\n') 
+        elif reply == '3':
+            break        
             
 
 login()
+
 
